04c6ec6b72c37666eaa2e0225bf7958a6acfedba
dangtung <language0512@gmail.com>
2023-04-10 15:10:26 +0700

    update role reducers

--- src/stores/reducers/role.reducer.js
+++ src/stores/reducers/role.reducer.js
@@ -1,1 +1,25 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+
+export default createSlice({
+    name: "roles",
+
+    initialState: [
+        { id: 1, name: "admin", isActive: true },
+        { id: 2, name: "supper admin", isActive: true },
+        { id: 3, name: "member", isActive: true },
+    ],
+
+    reducers: {
+        addRole: (state, action) => {
+            state.push(action.payload);
+        },
+
+        toogleActiveStatus: (state, action) => {
+            const curentRole = state.find(role => role.id === action.payload);
+            if (curentRole) {
+                curentRole.isActive = !curentRole.isActive;
+            }
+        }
+    }
+
+})
