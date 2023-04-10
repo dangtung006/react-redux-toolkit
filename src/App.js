a8f37ef63dedff7fe0abaafd4711241f6723f54f
dangtung <language0512@gmail.com>
2023-04-10 14:06:09 +0700

    update redux stores

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import "./App.css"
 import { BrowserRouter } from 'react-router-dom'
 import Router from './router';
 import BaseLayout from './layouts/base';
-
+import stores from "./stores/stores";
+import { Provider } from "react-redux";
 
 function App() {
     return (
         <div className="App">
-            <BrowserRouter>
-                <BaseLayout>
-                    <Router />
-                </BaseLayout>
-            </BrowserRouter>
+            <Provider store={stores}>
+                <BrowserRouter>
+                    <BaseLayout>
+                        <Router />
+                    </BaseLayout>
+                </BrowserRouter>
+            </Provider>
         </div>
     );
 }
---
+++ src/stores/stores.js
@@ -0,0 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+
+const stores = configureStore({
+    reducer: {
+
+    }
+})
+
+export default stores;
