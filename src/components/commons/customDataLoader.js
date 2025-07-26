1e9294cc7f277530702cf933bcd4303fc6b29db3
Tung Dang <tungds@vimo.vn>
2025-07-26 17:37:03 +0700

    update customLoader

--- src/components/commons/customDataLoader.js
+++ src/components/commons/customDataLoader.js
@@ -1,1 +1,9 @@
 import React from "react";
+
+const CustomDataLoader = ({ status, error, children, renderLoading, renderError }) => {
+    if (status === 'loading') return renderLoading?.() || <div>Loading...</div>;
+    if (status === 'failed') return renderError?.(error) || <div>Error: {error}</div>;
+    return <>{children}</>;
+}
+
+export default CustomDataLoader;
